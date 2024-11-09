22af9a920810dc988f773a4678bcc3ae8fbd2170
TisoczkiBenedek <benedektisoczki@gmail.com>
2024-11-09 15:06:35 +0100

    sql fuggvenyek

---
+++ php/sql_fuggvenyek.php
@@ -0,0 +1,55 @@
+<?php
+function adatokLekerese($muvelet){
+    //Kapcsolat létrehozása
+    $db = new mysqli ('localhost', 'root', '', 'vizsgaremek');
+    // Kapcsolat létrejött-e
+    if($db->connect_errno == 0){
+        //Az sql művelet végrehajtása
+        $eredmeny = $db->query($muvelet);
+        //Történt-e hiba a végrehajtáskor
+        if($db->errno == 0){
+            //Kaptunk-e vissza adatokat
+            if($eredmeny->num_rows !=0){
+                //Az adatok lehívása
+                $adatok = $eredmeny->fetch_all(MYSQLI_ASSOC);
+                return $adatok;
+            }
+            else {
+                return 'Nincsenek találatok!';
+            }
+        }
+        else{
+            return $db->error;
+        }
+    }
+    else{
+        return $db->connect_error;
+    }
+}
+function adatokValtoztatasa($muvelet){
+    $db = new mysqli ('localhost', 'root', '', 'vizsgaremek');
+    if($db->connect_errno==0){
+        $db->query($muvelet);
+        if($db-> errno==0){
+            if($db->affected_rows>0){
+                return 'Sikeres művelet!';
+            }
+            else if($db->affected_rows==0){
+                return 'Sikertelen művelet!';
+            }
+            else{
+                return $db->error;
+            }
+        }
+        else{
+            return $db->error;
+        }
+    }
+    else{
+        return $db->connect_error;
+    }
+}
+
+
+
+?>
