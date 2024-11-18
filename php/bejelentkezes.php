fe947f1c76b1698901cf59e18147e973a758f577
TisoczkiBenedek <benedektisoczki@gmail.com>
2024-11-18 20:53:55 +0100

    Csabi kódjának beillesztése

--- php/bejelentkezes.php
+++ php/bejelentkezes.php
@@ -15,6 +15,8 @@ function bejelentkezes(){
     $email = $adatok['email'];
     $muvelet = "SELECT felhasznalo.emailcim, felhasznalo.tulajdonos, felhasznalo.takarito, felhasznalo.jelszo FROM felhasznalo WHERE felhasznalo.emailcim like '{$email}'";
     $eredmeny = adatokLekerese($muvelet);
+    $_SESSION['emailcim'] = $eredmeny[0]['emailcim'];//email cím átküldése a főoldalra, a felhasználónév betöltésésnek céljából.
+    header('Location: adminFoOldal.php');//átküldés az admin oldalra
     if(is_array($eredmeny)){
         if(password_verify($adatok['jelszo'], $eredmeny[0]['jelszo'])){
             echo json_encode($eredmeny, JSON_UNESCAPED_UNICODE);
