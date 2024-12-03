260f73a7c2dd091b1c95af0371b61e9c8128bb67
GrezsuCsaba <8200grezsucsaba@gmail.com>
2024-12-03 11:16:33 +0100

    12.03óra

---
+++ adminOldal/adminOldalEszkozok/js/adminOldalEszkozok.js
@@ -0,0 +1,21 @@
+// JavaScript a felugró ablak kezeléséhez 
+const felvitel = document.getElementById("felvitel"); 
+const eszkozFelvitel = document.getElementById("eszkozFelvitele"); 
+const span = document.getElementsByClassName("close")[0]; 
+
+
+eszkozFelvitel.onclick = function() { 
+    felvitel.style.display = "block"; 
+} 
+
+
+span.onclick = function() { 
+    felvitel.style.display = "none"; 
+} 
+
+
+window.onclick = function(event) { 
+    if (event.target == felvitel) { 
+        felvitel.style.display = "none"; 
+    } 
+}
