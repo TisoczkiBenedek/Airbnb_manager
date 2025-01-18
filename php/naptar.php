2c2075aac8ebd2c3d68a53e98b70647f64616b64
grezsucsaba <8200grezsucsaba@gmail.com>
2025-01-18 17:04:50 +0100

    fájlok2

--- php/naptar.php
+++ php/naptar.php
@@ -4,29 +4,3 @@ include 'sql_fuggvenyek.php';
 
 ?>
 
-<!DOCTYPE html>
-<html lang="hu">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/lakastulaj.css">
-    <script src="../js/lakastulaj.js" defer></script>
-    <title>Document</title>
-</head>
-<body>
-    <div id="calendar-container"> 
-        <div id="calendar-header"> 
-            <svg id="prev-month" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
-            </svg>
-            <h2 id="calendar-title"></h2> 
-            <svg id="next-month" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-            </svg>
-        </div> 
-        <div id="week"></div>
-        <div id="calendar">
-        </div>
-    </div>
-</body>
-</html>
