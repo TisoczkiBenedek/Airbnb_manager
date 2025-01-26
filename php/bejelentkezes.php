e003dc787d689ece0363f3dd289499a8a671f320
grezsucsaba <8200grezsucsaba@gmail.com>
2025-01-26 10:24:52 +0100

    új lakás oldal

--- php/bejelentkezes.php
+++ php/bejelentkezes.php
@@ -14,30 +14,30 @@ function bejelentkezes(){
     $adatok = json_decode(file_get_contents('php://input'), true);
     if(!empty($adatok['email']) && isset($adatok['email'])){
         $email = $adatok['email'];
-    $muvelet = "SELECT felhasznalo.emailcim, felhasznalo.tulajdonos, felhasznalo.takarito, felhasznalo.jelszo FROM felhasznalo WHERE felhasznalo.emailcim like '{$email}'";
-    $eredmeny = adatokLekerese($muvelet);
-    if(is_array($eredmeny)){
-        if(password_verify($adatok['jelszo'], $eredmeny[0]['jelszo'])){
-            $_SESSION['emailcim'] = $eredmeny[0]['emailcim'];//email cím átküldése a főoldalra, a felhasználónév betöltésésnek céljából.
-            //echo json_encode($eredmeny, JSON_UNESCAPED_UNICODE);
+        $muvelet = "SELECT felhasznalo.emailcim, felhasznalo.tulajdonos, felhasznalo.takarito, felhasznalo.jelszo FROM felhasznalo WHERE felhasznalo.emailcim like '{$email}'";
+        $eredmeny = adatokLekerese($muvelet);
+        if(is_array($eredmeny)){
+            if(password_verify($adatok['jelszo'], $eredmeny[0]['jelszo'])){
+                $_SESSION['emailcim'] = $eredmeny[0]['emailcim'];//email cím átküldése a főoldalra, a felhasználónév betöltésésnek céljából.
+                //echo json_encode($eredmeny, JSON_UNESCAPED_UNICODE);
 
-            if($eredmeny[0]['tulajdonos'] == 1){//felhasználó típusának ellenőrzése(tulaj)
-                $_SESSION['felhTipus'] = 'tulajdonos';
-            } elseif($eredmeny[0]['takarito'] == 1) {
-                $_SESSION['felhTipus'] = 'takarito';
-            } else {
-                $_SESSION['felhTipus'] = 'admin';
+                if($eredmeny[0]['tulajdonos'] == 1){//felhasználó típusának ellenőrzése(tulaj)
+                    $_SESSION['felhTipus'] = 'tulajdonos';
+                } elseif($eredmeny[0]['takarito'] == 1) {
+                    $_SESSION['felhTipus'] = 'takarito';
+                } else {
+                    $_SESSION['felhTipus'] = 'admin';
+                }
+
+                echo json_encode([ 'emailcim' => $eredmeny[0]['emailcim'], 'felhTipus' => $_SESSION['felhTipus'] ], JSON_UNESCAPED_UNICODE);
             }
-
-            echo json_encode([ 'emailcim' => $eredmeny[0]['emailcim'], 'felhTipus' => $_SESSION['felhTipus'] ], JSON_UNESCAPED_UNICODE);
+            else{
+                echo json_encode(["valasz"=>"Hibás jelszó!"], JSON_UNESCAPED_UNICODE);
+            }
         }
         else{
-            echo json_encode(["valasz"=>"Hibás jelszó!"], JSON_UNESCAPED_UNICODE);
+            echo json_encode(["valasz"=>"Nem található ilyen emil cím és jelszó páros!"], JSON_UNESCAPED_UNICODE);
         }
-    }
-    else{
-        echo json_encode(["valasz"=>"Nem található ilyen emil cím és jelszó páros!"], JSON_UNESCAPED_UNICODE);
-    }
     }
     else{
         echo json_encode(["valasz"=>"Nem hiányos adatok!"], JSON_UNESCAPED_UNICODE);
