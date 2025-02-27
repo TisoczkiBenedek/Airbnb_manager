1f09fac3dbc7b79c134d3b6d8751b73264dc3c1e
TisoczkiBenedek <benedektisoczki@gmail.com>
2025-02-27 18:39:27 +0100

    Bejelentkezés visszajelzések javítása

--- js/bejelentkezes.js
+++ js/bejelentkezes.js
@@ -1,56 +1,84 @@
-function ellenorzes(){
-    let emailcim = document.getElementById('email').value 
-    let jelszo = document.getElementById('jelszo').value 
-    if(email = "" || jelszo== ""){
-        alert("Kérem minden mezőt töltsön ki!")
-        return
-    }
-    else if(!emailcim.includes("@")){
-        alert("Hibás e-mail cím!")
+function ellenorzes() {
+    let emailcim = document.getElementById('email').value
+    let jelszo = document.getElementById('jelszo').value
+    let vhely = document.getElementById("vhely")
+    vhely.innerText = ""
+    if (email = "" || jelszo == "") {
+        vhely.innerText = "Kérem minden mezőt töltsön ki!"
         document.getElementById('email').style.border = "2px dashed red"
+        document.getElementById('jelszo').style.border = "2px dashed red"
+        vhely.hidden = false
         return
     }
-    else if(jelszo.length<8){
-        alert("Túl rövid jelszó!")
+    else if (!emailcim.includes("@")) {
+        vhely.innerText = "Hibás e-mail cím!"
         document.getElementById('email').style.border = "2px dashed red"
+        document.getElementById('jelszo').style.border = "none"
+        vhely.hidden = false
         return
     }
-    else{
+    else if (jelszo.length < 8) {
+        vhely.innerText = "Túl rövid jelszó!"
+        document.getElementById('jelszo').style.border = "2px dashed red"
+        document.getElementById('email').style.border = "none"
+        vhely.hidden = false
+        return
+    }
+    else {
         adatKuldes(emailcim, jelszo)
+        document.getElementById('jelszo').style.border = "none"
+        document.getElementById('email').style.border = "none"
+        vhely.hidden = true
     }
 }
 async function adatKuldes(email, jelszo) {
     try {
         let kuldendo = {
-            "email": email, 
+            "email": email,
             "jelszo": jelszo
         }
         let eredmeny = await fetch('../php/bejelentkezes.php/bejelentkezes', {
-            method : "POST",
-            headers : {
+            method: "POST",
+            headers: {
                 "Content-Type": "application/json"
             },
-            body : JSON.stringify(kuldendo)
+            body: JSON.stringify(kuldendo)
         })
-        if(eredmeny.ok){
-            let valasz = await eredmeny.json()
-            felhasznaloInformalas(valasz)
-        }
-        else{
-            throw new error
-        }
+
+        let adatok = await eredmeny.json()
+        felhasznaloInformalas(adatok)
+
+
     } catch (error) {
         console.log(error)
     }
 }
-function felhasznaloInformalas(valasz){
-    if(Array.isArray(valasz)){
-        console.log(valasz)
-        //window.open('../html/adminproba.html', '_parent')
-        window.open('../adminOldal/adminOldalEszkozok/php/adminOldalEszkozok.php', '_parent')
+function felhasznaloInformalas(adatok) {
+    let vhely = document.getElementById("vhely")
+    if (adatok.valasz) {
+        vhely.innerText = adatok["valasz"]
+        vhely.hidden = false
     }
-    else{
-        console.log(valasz)
+    else {
+        console.log(adatok["felhTipus"])
+        if (adatok.felhTipus) {
+            console.log(adatok)
+
+            const felhTipus = adatok.felhTipus;
+
+            if (felhTipus == 'tulajdonos') {
+                window.open('../html/lakasok.html', '_parent');
+            } else {
+                window.open('../html/takarito.html', '_parent');
+            }
+        }
+    }
+}
+function enterBevitel(event) {
+    if (event.key === "Enter") {
+        ellenorzes()
     }
 }
 document.getElementById('gomb').addEventListener('click', ellenorzes)
+document.getElementById('jelszo').addEventListener("keypress", enterBevitel)
+document.getElementById('email').addEventListener("keypress", enterBevitel)
