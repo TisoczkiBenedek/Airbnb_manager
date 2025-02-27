f64813053051842d3c2fcfa3aa3544c31fb16edc
grezsucsaba <8200grezsucsaba@gmail.com>
2025-02-27 22:14:22 +0100

    kijelentkezés + naptár css

--- js/lakastulaj.js
+++ js/lakastulaj.js
@@ -177,3 +177,27 @@ function loadProfilNev() {
 
 // Oldal betöltésekor futtatjuk
 document.addEventListener('DOMContentLoaded', loadProfilNev);
+
+async function kijelentkezes(){
+    try {
+        const eredmeny = await fetch('../php/kijelentkezes.php', {
+            method: 'POST',
+            credentials: 'include'
+        }); 
+
+        if(eredmeny.ok){
+            const adat = await eredmeny.json();
+
+            if(adat.success) {
+                window.location.href = '../html/bejelentkezes.html';
+            }else{
+                console.error('Hiba a kijelentkezés során:', adat.message);
+            }
+        }else{
+            throw new Error(`HTTP hiba! Státusz: ${eredmeny.status}`);
+        }
+    } catch (error) {
+        console.error('Hiba a kijelentkezés során: ', error)
+    }
+    
+}
