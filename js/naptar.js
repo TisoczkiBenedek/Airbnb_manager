946a822ed30106cf13776b6aa78afd02866ec957
grezsucsaba <8200grezsucsaba@gmail.com>
2025-03-19 22:00:13 +0100

    naptár alap

--- js/naptar.js
+++ js/naptar.js
@@ -8,22 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         buttonText: {
             today: 'Mai nap'
         },
-        eventContent: function(info) {
-            // Ha az esemény címe "Takarítás", akkor hozzáadjuk a kezdő és végző időpontokat
-            if (info.event.title === 'Takarítás') {
-                const startTime = info.event.start ? info.timeText : '';
-                const endTime = info.event.end ? info.event.end.toLocaleTimeString('hu-HU', { hour: '2-digit', minute: '2-digit' }) : '';
-                return {
-                    html: `
-                        <div class="fc-event-takaritas">
-                            <div class="fc-event-title">${info.event.title}</div>
-                            <div class="fc-event-time">${startTime} - ${endTime}</div>
-                        </div>
-                    `
-                };
-            }
-            // Egyéb események esetén alapértelmezett megjelenítés
-            return { html: info.event.title };
-        },
     })
+
+    calendar.render()
 })
